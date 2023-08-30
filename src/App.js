eaeb5cd2f5cf8f4a64c814d4a8c9b7b6eab67ca7
simonsajjizoft <simon.sajimathews@zoftsolutions.com>
2023-08-30 23:10:19 +0530

    React Light dark theme with context api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Posts from "./components/Posts";
+import { ThemeContext, themes } from "./context/themeContext";
 
 function App() {
+  const [theme, setTheme] = useState(themes.light);
+
+  //btn
+  function handleOnClick() {
+    theme === themes.light ? setTheme(themes.dark) : setTheme(themes.light);
+  }
+
+  const body = document.body;
+  useEffect(() => {
+    switch (theme) {
+      case themes.light:
+        body.classList.remove("bg-dark");
+        body.classList.remove("text-light");
+        body.classList.add("bg-light");
+        body.classList.add("text-dark");
+        break;
+      case themes.dark:
+        body.classList.remove("bg-light");
+        body.classList.remove("text-dark");
+        body.classList.add("bg-dark");
+        body.classList.add("text-light");
+        break;
+      default:
+        body.classList.remove("bg-dark");
+        body.classList.remove("text-light");
+        body.classList.add("bg-light");
+        body.classList.add("text-dark");
+    }
+  }, [theme]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ThemeContext.Provider value={{ theme, handleOnClick }}>
+        <div className="main-container">
+          <h1 className="text-center">Light Dark Theme App</h1>
+          <Posts theme={theme} />
+        </div>
+      </ThemeContext.Provider>
+    </>
   );
 }
 
